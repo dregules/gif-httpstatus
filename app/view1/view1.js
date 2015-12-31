96e79d70ef25589c3e3a97276e58b40acc7d7f43
Diego Regules <diegoregules@gmail.com>
2015-12-31 21:25:29 +0100

    added gifs categories

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -19,9 +19,32 @@ angular.module('myApp.view1', ['ngRoute'])
         $scope.getRandomGif();
       });
   };
+  var statusMap = {
+    '1xx': {
+      gifTag: "american+psycho",
+      category: "Informational Response"
+    },
+    '2xx': {
+      gifTag: "success",
+      category: "Successful Response"
+    },
+    '3xx': {
+      gifTag: "funny+cat",
+      category: "Redirection Message"
+    },
+    '4xx': {
+      gifTag: "shit",
+      category: "Client Error Response"
+    },
+    '5xx': {
+      gifTag: "error",
+      category: "Server Error Response"
+    }
+  };
   $scope.getRandomGif = function(){
-    $http.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho')
+    $http.get('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=shit')
       .success(function(response) {
+        console.log(response.data);
         $scope.randomGiphy = response.data.image_original_url;
       });
   };
