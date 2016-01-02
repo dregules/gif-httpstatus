537261dc27bdc827ffbac36e15e9aa3259ca050e
Diego Regules <diegoregules@gmail.com>
2016-01-02 01:56:31 +0100

    implemented references in views,changed ngmodel

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -3,19 +3,26 @@
 angular.module('myApp.view1', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  });
+  $routeProvider
+    .when('/view1', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    })
+    .when('/view1/:status', {
+      templateUrl: 'view1/view1.html',
+      controller: 'View1Ctrl'
+    });
 }])
 
 
 
-.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+.controller('View1Ctrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+  $scope.statusSearch = {};
   $scope.findError = function(){
-    $http.get('/app/data/errors.json')
+    $http.get('/app/data/statusHttp.json')
       .success(function(response) {
         $scope.errors = response;
+        console.log("Heres the 'params': " , $scope.statusSearch);
         $scope.getRandomGif();
       });
   };
